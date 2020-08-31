d5cac1fc1b0c2cc6c4599aba99a5ae9564da8615
Lucas <lucas.moreno.olivos@gmail.com>
2020-08-31 17:45:30 -0500

    azulejos terminado

--- semana3/azulejos.py
+++ semana3/azulejos.py
@@ -1,5 +1,6 @@
 import random
 import math
+from PIL import Image, ImageDraw
 color = 0
 def azulejos():
   global color
@@ -35,8 +36,19 @@ def azulejos():
     divide(xo, infxn, supyn, yn)
     divide(supxn, xn, supyn, yn)
   divide(0, lado - 1, 0, lado - 1)
-  for x in piso:
-    for y in x:
-      print(f'{y}\t', end="")
-    print('')      
+  colores = []
+  for x in range(color):
+    colores.append({
+        'red': random.randint(0, 255),
+        'green' : random.randint(0, 255),
+        'blue' : random.randint(0, 255)
+    })
+  tam = int(input("inserta el tamaño de los cuadrados: "))    
+  im = Image.new('RGB', (tam*lado, tam*lado), (128, 128, 128))
+  draw = ImageDraw.Draw(im)
+  for x, a in enumerate(piso):
+    for y, b in enumerate(a):
+      draw.rectangle((x*tam, y*tam,tam*(x + 1), tam*(y + 1)), fill=(colores[b - 1]['red'], colores[b - 1]['green'], colores[b - 1]['blue']), outline=(colores[b - 1]['red'], colores[b - 1]['green'], colores[b - 1]['blue']))
+  name = input("Ingresa el nombre que le pondras a tu imagen:")
+  im.save(f'{name}.jpg', quality=95)      
 azulejos()
